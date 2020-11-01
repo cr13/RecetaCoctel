6b15eb72e260c48ece9ae5e2711203ff3962d7b6
Cristóbal <cristobal111992@gmail.com>
2020-11-01 21:48:50 +0100

    Se añade clase ingrediente con definicion de metodos #13 #5

---
+++ src/Ingredientes.js
@@ -0,0 +1,36 @@
+
+// Definición de la clase Ingredientes:
+
+exports.Ingredientes = function (nombre, descripcion, tipo, receta) {
+	this.nombre = nombre;
+	this.descripcion = descripcion;
+	this.tipo = tipo;
+	this.receta = receta;
+
+	this.set_ingrediente = set_ingrediente;
+	this.get_ingrediente = get_ingrediente;
+	this.modi_ingrediente = modi_ingrediente;
+	this.assign_receta = assign_receta;
+	this.get_recetas = get_recetas;
+
+};
+
+
+function set_ingrediente() {
+	return true;
+}
+
+function get_ingrediente() {
+	return true;
+}
+
+function modi_ingrediente() {
+	return true;
+}
+function assign_receta() {
+	return true;
+}
+
+function get_recetas() {
+	return true;
+}
