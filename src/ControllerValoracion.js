2ef356d94007a5251257c35a4f5914be2fa8d1b5
Cristóbal <cristobal111992@gmail.com>
2020-11-11 18:44:03 +0100

    Añado la clase Controladorvaloracion la cual se encargara de las funcionalidades respecto a las valoraciones de recetas #4 #13 #9

---
+++ src/ControllerValoracion.js
@@ -0,0 +1,18 @@
+
+var receta = require("./Receta");
+var val_receta = require("./ValoracionReceta");
+
+
+class ControllerValoracion {
+    constructor() {
+        this.valoraciones_recetas = {};
+    }
+
+    //Funcion para añadir valoraciones a las recetas
+    addValoracionReceta(receta, val) {
+
+    }
+
+}
+
+module.exports = ControllerValoracion;
