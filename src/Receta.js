ef24902457c88142d13b368abcbaca551d5b8304
Cristóbal <cristobal111992@gmail.com>
2020-11-11 01:43:09 +0100

    Modifico nombre de atributo de la clase  descripcion por instrucciones y comentario de funcion#13

--- src/Receta.js
+++ src/Receta.js
@@ -2,16 +2,16 @@
 
 // Definición de la clase Receta:
 
-exports.Receta = function (titulo, descripcion, ingredientes) {
+exports.Receta = function (titulo, instrucciones, ingredientes) {
 	this.titulo = titulo;
-	this.descripcion = descripcion;
+	this.instrucciones = instrucciones;
 	this.ingredientes = ingredientes;
 
 	this.get_receta = get_receta;
 
 };
 
-// Función que devuelve una receta
-function get_receta() {
-	return {"titulo":titulo, "descripcion":descripcion, "ingredientes":ingredientes};
+// Función que devuelve una receta como diccionario
+function get_dict_receta() {
+	return { "titulo": titulo, "instrucciones": instrucciones, "ingredientes":ingredientes};
 }
