6aba1f87ae8167e839bd096a047c37007120e101
Cristóbal <cristobal111992@gmail.com>
2020-11-11 18:51:45 +0100

    Arreglo sintactico en la definicion de la clase Ingrediente  #4 #13

--- src/Ingrediente.js
+++ src/Ingrediente.js
@@ -1,19 +1,22 @@
 
 // Definición de la clase Ingrediente
 
-exports.Ingrediente = function (nombre, descripcion, tipo, recetas) {
-	this.nombre = nombre;
-	this.descripcion = descripcion;
-	this.tipo = tipo;
-	this.recetas = recetas;
+class Ingrediente {
+	constructor (nombre, descripcion, tipo, recetas) {
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+		this.tipo = tipo;
+		this.recetas = recetas;
 
-	this.assign_receta = assign_receta;
+		this.assign_receta = assign_receta;
 
-};
+	};
 
-// Asignar ingrediente a receta
-function assign_receta(receta) {
-	this.recetas.push(receta);
+	// Asignar ingrediente a receta
+	assign_receta(receta) {
+		this.recetas.push(receta);
+	}
+
 }
 
 module.exports = Ingrediente;
