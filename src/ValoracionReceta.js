3ad8132aa5ee13a904d0d021dbbb724f260bc398
Cristóbal <cristobal111992@gmail.com>
2020-11-11 18:52:44 +0100

    Arreglo sintactico en la definicion de la clase Valoracion  #4 #13

--- src/ValoracionReceta.js
+++ src/ValoracionReceta.js
@@ -2,14 +2,15 @@
 
 // Definición de la clase ValoracionReceta:
 
-exports.ValoracionReceta = function (user, receta, valoracion) {
-    this.user = user;
-    this.receta = receta;
-    this.valoracion = valoracion;   
+class ValoracionReceta {
+    constructor(user, receta, valoracion) {
+        this.user = user;
+        this.receta = receta;
+        this.valoracion = valoracion;   
 
-    this.set_valReceta = set_valReceta;
+        this.set_valReceta = set_valReceta;
 
-};
-
+    };
+}
 
 module.exports = ValoracionReceta;
