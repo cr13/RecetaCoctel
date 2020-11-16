06109886eb68fe50fdfe9b1dff85102bec7ab30d
Cristóbal <cristobal111992@gmail.com>
2020-11-16 02:13:12 +0100

    Actualizo test para comprobar que no se repita una receta #15 #19 esta relacionado con la historia de usuario insertar receta #6

--- src/test/receta.test.js
+++ src/test/receta.test.js
@@ -2,16 +2,17 @@ var receta = require("../Receta");
 var ingrediente = require("../Ingrediente");
 
 describe("Función para insertar una receta", () => {
-    test("debe comprobar que no se repiten un mismo ingrediente en la receta", () => {
+    test("debe comprobar que nos se repita la receta", () => {
+        // const ingrediente_uno = new ingrediente.Ingrediente("Ron Blanco", "100ml", "bebida");
         const ingrediente_uno = new ingrediente("Ron Blanco", "100ml", "bebida");
         const ingrediente_dos = new ingrediente("Seven UP", "200ml", "bebida");
         const ingrediente_tres = new ingrediente("Azucar Morena", "1 o 2 cucharadas", "Hidratos de carbono");
-        const receta_uno = new receta("Mojito", "Tritura las hojas de menta con el azúcar y el zumo de lima. Agrega un chorrito de agua con gas y llena el vaso con hielo picado. Vierta el ron y cubra con agua con gas. Decore y sirva con pajita.", [ingrediente_uno, ingrediente_dos, ingrediente_tres] );
+        const ingrediente_cuatro = new ingrediente("Hierba buena", "2-4 hojas", "Flor");
 
-        //const output = receta_uno;
+        const receta_uno = new receta("Mojito", "Tritura las hojas de menta con el azúcar y el zumo de lima. Agrega un chorrito de agua con gas y llena el vaso con hielo picado. Vierta el ron y cubra con agua con gas. Decore y sirva con pajita.", [ingrediente_uno, ingrediente_dos, ingrediente_tres]);
+        const receta_dos = new receta("Mojito", "Tritura las hojas de menta con el azúcar y el zumo de lima. Agrega un chorrito de agua con gas y llena el vaso con hielo picado. Vierta el ron y cubra con agua con gas. Decore y sirva con pajita.", [ingrediente_uno, ingrediente_dos, ingrediente_tres, ingrediente_cuatro]);
 
-        expect(ingrediente_uno).not.toBe(ingrediente_dos);
-        expect(ingrediente_uno).not.toBe(ingrediente_tres);
-        expect(ingrediente_dos).not.toBe(ingrediente_tres);
+        expect(receta_uno).not.toBe(receta_dos);
+
     });
 });
