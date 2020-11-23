e57d6909ffae6f25db4118b982f520d386e178bd
Cristóbal <cristobal111992@gmail.com>
2020-11-23 16:59:33 +0100

    Se elimina console log del test controller_receta #19 #15

--- src/test/controller_receta.test.js
+++ src/test/controller_receta.test.js
@@ -46,7 +46,6 @@ describe("Función para obtener una receta por titulo", () => {
 
         expect.assertions(listRecetas);
         listRecetas.addReceta(receta_uno);
-        console.log(listRecetas.getRecetaByTitle(listRecetas.getRecetas(), 'Mojito'));
         expect(listRecetas.getRecetaByTitle(listRecetas.getRecetas(), 'Mojito')).toEqual(
             expect.objectContaining([{ "comensales": 5, "dificultad": "Fácil", "duracion": 10, "ingredientes": [{ "descripcion": "100ml", "nombre": "Ron Blanco", "recetas": undefined, "tipo": "bebida" }, { "descripcion": "200ml", "nombre": "Seven UP", "recetas": undefined, "tipo": "bebida" }, { "descripcion": "1 o 2 cucharadas", "nombre": "Azucar Morena", "recetas": undefined, "tipo": "Hidratos de carbono" }], "instrucciones": "Tritura las hojas de menta con el azúcar y el zumo de lima. Agrega un chorrito de agua con gas y llena el vaso con hielo picado. Vierta el ron y cubra con agua con gas. Decore y sirva con pajita.", "titulo": "Mojito" }]))
 
