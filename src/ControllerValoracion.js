76272de2eab065007388830f24acd3de7bd648ce
Cristóbal <cristobal111992@gmail.com>
2020-11-23 16:35:16 +0100

    se asocia la clase valoracionReceta #19 #26

--- src/ControllerValoracion.js
+++ src/ControllerValoracion.js
@@ -4,14 +4,7 @@ var val_receta = require("./ValoracionReceta");
 
 
 class ControllerValoracion {
-    constructor() {
-        this.valoraciones_recetas = {};
-    }
 
-    //Funcion para añadir valoraciones a las recetas
-    addValoracionReceta(receta, val) {
-
-    }
 
 }
 
