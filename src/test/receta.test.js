a311e336cf8863df5748edd98d76f94f3e00dc05
Cristóbal <cristobal111992@gmail.com>
2020-11-23 16:56:41 +0100

    Se corrige test receta #19 #15

--- src/test/receta.test.js
+++ src/test/receta.test.js
@@ -9,8 +9,10 @@ describe("Función para insertar una receta", () => {
         const ingrediente_tres = new ingrediente("Azucar Morena", "1 o 2 cucharadas", "Hidratos de carbono");
         const ingrediente_cuatro = new ingrediente("Hierba buena", "2-4 hojas", "Flor");
 
-        const receta_uno = new receta("Mojito", "Tritura las hojas de menta con el azúcar y el zumo de lima. Agrega un chorrito de agua con gas y llena el vaso con hielo picado. Vierta el ron y cubra con agua con gas. Decore y sirva con pajita.", [ingrediente_uno, ingrediente_dos, ingrediente_tres]);
-        const receta_dos = new receta("Mojito", "Tritura las hojas de menta con el azúcar y el zumo de lima. Agrega un chorrito de agua con gas y llena el vaso con hielo picado. Vierta el ron y cubra con agua con gas. Decore y sirva con pajita.", [ingrediente_uno, ingrediente_dos, ingrediente_tres, ingrediente_cuatro]);
+        const receta_uno = new receta("Mojito", "Tritura las hojas de menta con el azúcar y el zumo de lima. Agrega un chorrito de agua con gas y llena el vaso con hielo picado. Vierta el ron y cubra con agua con gas. Decore y sirva con pajita.",
+        10, "Fácil", 5, [ingrediente_uno, ingrediente_dos, ingrediente_tres]);
+        const receta_dos = new receta("Mojito", "Tritura las hojas de menta con el azúcar y el zumo de lima. Agrega un chorrito de agua con gas y llena el vaso con hielo picado. Vierta el ron y cubra con agua con gas. Decore y sirva con pajita.",
+        15, "Fácil", 5, [ingrediente_uno, ingrediente_dos, ingrediente_tres, ingrediente_cuatro]);
 
         expect(receta_uno).not.toBe(receta_dos);
 
