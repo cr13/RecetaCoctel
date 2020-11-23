777087c7c0ae5ce7c66d4762ceee8648721f5d5d
Cristóbal <cristobal111992@gmail.com>
2020-11-23 16:37:53 +0100

    Se añade constructor a la clase ValoracionReceta #19 #25

--- src/ValoracionReceta.js
+++ src/ValoracionReceta.js
@@ -3,7 +3,12 @@
 // Definición de la clase ValoracionReceta:
 
 class ValoracionReceta {
-  
+    constructor(user, receta, valoracion) {
+        this.user = user;
+        this.receta = receta;
+        this.valoracion = valoracion;   
+
+    }
 }
 
 module.exports = ValoracionReceta;
