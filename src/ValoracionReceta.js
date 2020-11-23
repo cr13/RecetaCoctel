76c184938521a6f236d648d6c4890c49bdb70244
Cristóbal <cristobal111992@gmail.com>
2020-11-23 16:37:23 +0100

    se asocia la clase entidad valoracionREceta #19 #25

--- src/ValoracionReceta.js
+++ src/ValoracionReceta.js
@@ -3,12 +3,7 @@
 // Definición de la clase ValoracionReceta:
 
 class ValoracionReceta {
-    constructor(user, receta, valoracion) {
-        this.user = user;
-        this.receta = receta;
-        this.valoracion = valoracion;   
-
-    }
+  
 }
 
 module.exports = ValoracionReceta;
