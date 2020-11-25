7bb6ff70b1e3246da85aae3a8bf3b71ccc021108
Cristóbal <cristobal111992@gmail.com>
2020-11-25 01:58:39 +0100

    Se corrigen errores de sintaxis test controller receta #19 #23 #15

--- src/test/controller_receta.test.js
+++ src/test/controller_receta.test.js
@@ -23,7 +23,7 @@ describe("Función para añadir una receta", () => {
         expect(listRecetas.getRecetas().length).toEqual(1);
 
 
-    })
+    });
 });
 
 describe("Función para eliminar una receta", () => {
@@ -38,7 +38,7 @@ describe("Función para eliminar una receta", () => {
         expect(listRecetas.getRecetas().length).toEqual(1);
 
 
-    })
+    });
 });
 
 describe("Función para obtener una receta por titulo", () => {
@@ -47,8 +47,8 @@ describe("Función para obtener una receta por titulo", () => {
         expect.assertions(listRecetas);
         listRecetas.addReceta(receta_uno);
         expect(listRecetas.getRecetaByTitle(listRecetas.getRecetas(), 'Mojito')).toEqual(
-            expect.objectContaining([{ "comensales": 5, "dificultad": "Fácil", "duracion": 10, "ingredientes": [{ "descripcion": "100ml", "nombre": "Ron Blanco", "recetas": undefined, "tipo": "bebida" }, { "descripcion": "200ml", "nombre": "Seven UP", "recetas": undefined, "tipo": "bebida" }, { "descripcion": "1 o 2 cucharadas", "nombre": "Azucar Morena", "recetas": undefined, "tipo": "Hidratos de carbono" }], "instrucciones": "Tritura las hojas de menta con el azúcar y el zumo de lima. Agrega un chorrito de agua con gas y llena el vaso con hielo picado. Vierta el ron y cubra con agua con gas. Decore y sirva con pajita.", "titulo": "Mojito" }]))
+            expect.objectContaining([{ "comensales": 5, "dificultad": "Fácil", "duracion": 10, "ingredientes": [{ "descripcion": "100ml", "nombre": "Ron Blanco", "recetas": undefined, "tipo": "bebida" }, { "descripcion": "200ml", "nombre": "Seven UP", "recetas": undefined, "tipo": "bebida" }, { "descripcion": "1 o 2 cucharadas", "nombre": "Azucar Morena", "recetas": undefined, "tipo": "Hidratos de carbono" }], "instrucciones": "Tritura las hojas de menta con el azúcar y el zumo de lima. Agrega un chorrito de agua con gas y llena el vaso con hielo picado. Vierta el ron y cubra con agua con gas. Decore y sirva con pajita.", "titulo": "Mojito" }]));
 
 
-    })
+    });
 });
