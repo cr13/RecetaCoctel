2386b27ad7377735234c932dcd3687b2dbf98cf6
Cristóbal <cristobal111992@gmail.com>
2021-01-17 23:40:24 +0100

    se añade como buenas practicas etcd3 y uso como logs morgan

--- app.js
+++ app.js
@@ -1,11 +1,37 @@
 const express = require('express')
 const app = express()
-const port = 3001
+// const port = 3001
+const morgan = require('morgan');
+const dataRoutes = require('./routes/recetas');
 
-app.get('/', (req, res) => {
-  res.send('Welcome to RecetasCoctel!')
+const { Etcd3 } = require('etcd3');
+
+const client = new Etcd3();
+
+let port = process.env.port || 3010;
+app.set('json spaces', 2);
+
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+async function getPort() {
+  const port = await client.get('RecetaAppPort');
+  return port;
+}
+
+
+//Routes
+app.use(dataRoutes);
+
+(async () => {
+  port = await getPort();
+})().then(() => {
+
+  app.listen(port, function () {
+    console.log(`RecetasCoctel, escuchando en http://localhost:${port}/recetas`);
+  });
+
 })
 
-app.listen(port, () => {
-  console.log(`RecetasCoctel app listening at http://localhost:${port}`)
-})
+module.exports = app;
