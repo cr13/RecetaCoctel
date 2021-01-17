f51a60ffe13dc51fc5f6d48740338dd2abd6a611
Cristóbal <cristobal111992@gmail.com>
2021-01-17 01:34:31 +0100

    Avanzo #24, se añade control de tipos de  ingrediente  #32

--- src/Ingrediente.js
+++ src/Ingrediente.js
@@ -2,12 +2,22 @@
 // Definición de la clase Ingrediente
 
 class Ingrediente {
-	constructor (nombre, descripcion, tipo, recetas) {
-		this.nombre = nombre;
-		this.descripcion = descripcion;
-		this.tipo = tipo;
-		this.recetas = recetas;
+	constructor(id_ing, nombre, descripcion, tipo, recetas) {
+		if (this.checkType(id_ing, nombre, descripcion, tipo, recetas)){
+			this.id_ing = id_ing;
+			this.nombre = nombre;
+			this.descripcion = descripcion;
+			this.tipo = tipo;
+			this.recetas = recetas;
+		}
+		else {
+			throw "Los datos no son correctos";
+		}
+	}
 
+	//Función para comprobar el tipo de datos recibidos
+	checkType(id_ing, nombre, descripcion, tipo, recetas) {
+		return ((typeof id_ing === 'number') && (typeof nombre === 'string') && (typeof descripcion === 'string') && (typeof tipo === 'string') && (Array.isArray(recetas)));				
 	}
 
 	// Asignar ingrediente a receta
