5d14a28aa9c2263447b1a82cca6b84b5cf447cf9
Cristóbal <cristobal111992@gmail.com>
2021-01-17 01:33:50 +0100

    Avanzo #6, se añade control de tipos de  Receta #32

--- src/Receta.js
+++ src/Receta.js
@@ -3,19 +3,34 @@
 
 class Receta {
 	
-	constructor(titulo, instrucciones, duracion, dificultad, comensales, ingredientes) {
-		this.titulo = titulo;
-		this.instrucciones = instrucciones;
-		this.comensales = comensales;
-		this.duracion = duracion;
-		this.dificultad = dificultad;
-		this.ingredientes = ingredientes;
+	constructor(id_receta, titulo, instrucciones, duracion, dificultad, comensales, ingredientes) {
+		if (this.checkType(id_receta, titulo, instrucciones, duracion, dificultad, comensales, ingredientes)){
+			this.id_receta = id_receta;
+			this.titulo = titulo;
+			this.instrucciones = instrucciones;
+			this.comensales = comensales;
+			this.duracion = duracion;
+			this.dificultad = dificultad;
+			this.ingredientes = ingredientes;
+		} else {
+			throw "Los datos insertados no son correctos";
+		}
 
+	}
+
+	//Función para comprobar el tipo de datos recibidos
+	checkType(id_receta, titulo, instrucciones, duracion, dificultad, comensales, ingredientes) {
+		let succesful = false;
+		if ((typeof id_receta === 'number') && (typeof titulo === 'string') && (typeof comensales === 'number') && (typeof instrucciones === 'string') && (typeof duracion === 'number') && (typeof dificultad === 'number') && (Array.isArray(ingredientes))){
+			succesful= true;
+		} else { succesful = false;}
+
+		return succesful
 	}
 
 	// Función que devuelve una receta como diccionario
 	get_dict_receta() {
-		return { "titulo": this.titulo, "instrucciones": this.instrucciones, "duracion": this.duracion, "dificultad":this.dificultad, "comensales":this.comensales, "ingredientes": this.ingredientes };
+		return { "id_receta": this.id_receta, "titulo": this.titulo, "instrucciones": this.instrucciones, "duracion": this.duracion, "dificultad":this.dificultad, "comensales":this.comensales, "ingredientes": this.ingredientes };
 	}
 
 }
