d03612e663d016b11151adad49b596af1ad1e6ad
Cristóbal <cristobal111992@gmail.com>
2021-01-17 02:00:06 +0100

    Avanzo #29, se crear clase para control de excepciones [skip ci]

---
+++ src/Exception.js
@@ -0,0 +1,19 @@
+
+class Excepcion {
+
+    constructor(tipo, msg) {
+        if(this.checkType(tipo, msg)){
+            this.tipo = tipo;
+            this.msg = msg;
+        } 
+        else {
+            throw "El tipo de datos insertado no son string";
+        }
+    }
+
+    //Función para comprobar el tipo de datos recibidos
+    checkType(tipo, msg) {
+        return ((typeof tipo === 'string') && (typeof msg === 'string'));
+    }
+
+module.exports = Excepcion;
