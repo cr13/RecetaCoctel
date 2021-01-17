98efc9427a050e118167b255ea6dc494b9107006
Cristóbal <cristobal111992@gmail.com>
2021-01-17 04:43:27 +0100

    Avanzo #24, se corrige test para añadir ingredientes desde clase controladora [skip ci]

--- src/test/controller_ingrediente.test.js
+++ src/test/controller_ingrediente.test.js
@@ -1,23 +1,21 @@
-let receta = require("../Receta");
 let ingrediente = require("../Ingrediente");
 let controllerIng = require("../ControllerIngrediente");
 
 let listIngredientes = new controllerIng();
 
 
-const ingrediente_uno = new ingrediente("Ron Blanco", "100ml", "bebida");
-const ingrediente_dos = new ingrediente("Seven UP", "200ml", "bebida");
-const ingrediente_tres = new ingrediente("Azucar Morena", "1 o 2 cucharadas", "Hidratos de carbono");
-const ingrediente_cuatro = new ingrediente("Hierba buena", "2-4 hojas", "Flor");
+const ingrediente_uno = new ingrediente(1,"Ron Blanco", "100ml", "bebida");
+const ingrediente_dos = new ingrediente(2,"Seven UP", "200ml", "bebida");
+const ingrediente_tres = new ingrediente(3,"Azucar Morena", "1 o 2 cucharadas", "Hidratos de carbono");
 
 
 describe("Función para añadir ingredientes", () => {
     test('debe añadir tres ingredientes y comprobamos que efectivamente el array contiene 3 elemento', () => {
 
         expect.assertions(listIngredientes);
-        listIngredientes.addIngrediente(new ingrediente("Ron Blanco", "100ml", "bebida"));
-        listIngredientes.addIngrediente(new ingrediente("Seven UP", "200ml", "bebida"));
-        listIngredientes.addIngrediente(new ingrediente("Hierba buena", "2-4 hojas", "Flor"));
+        listIngredientes.addIngrediente(ingrediente_uno);
+        listIngredientes.addIngrediente(ingrediente_dos);
+        listIngredientes.addIngrediente(ingrediente_tres);
         expect(listIngredientes.getIngs().length).toEqual(3);
 
 
