c6d451bb45db0ca7a9070eed0fadf27b15fa5d84
Cristóbal <cristobal111992@gmail.com>
2021-01-18 01:20:24 +0100

    quito el servicio de configuracion distribuida etcd3 app.js

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const app = express();
 const morgan = require('morgan');
 const dataRoutes = require('./routes/recetas');
 
-const { Etcd3 } = require('etcd3');
+// const { Etcd3 } = require('etcd3');
 
-const client = new Etcd3();
+// const client = new Etcd3();
 
 let port = process.env.port || 3010;
 app.set('json spaces', 2);
@@ -15,23 +15,27 @@ app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-async function getPort() {
-  const port = await client.get('RecetaAppPort');
-  return port;
-}
+// async function getPort() {
+//   const port = await client.get('RecetaAppPort');
+//   return port;
+// }
 
 
 //Routes
 app.use(dataRoutes);
 
-(async () => {
-  port = await getPort();
-})().then(() => {
+// (async () => {
+//   port = await getPort();
+// })().then(() => {
 
-  app.listen(port, function () {
-    console.log(`RecetasCoctel, escuchando en http://localhost:${port}/recetas`);
-  });
+//   app.listen(port, function () {
+//     console.log(`RecetasCoctel, escuchando en http://localhost:${port}/recetas`);
+//   });
 
-})
+// })
+
+app.listen(port, function () {
+  console.log(`RecetasCoctel, escuchando en http://localhost:${port}/recetas`);
+});
 
 module.exports = app;
