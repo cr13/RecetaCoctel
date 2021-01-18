213f570a6c5ce6770e5a0acfc3022b8c05dcedb3
Cristóbal <cristobal111992@gmail.com>
2021-01-18 02:54:33 +0100

    Se añade tarea grunt build en Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,14 +21,10 @@ module.exports = function (grunt) {
             },
             npm_test_jest: {
                 command: 'npm test',
-            }
-        },
-        'build-atom-shell': {
-            tag: 'v1.1.0',
-            nodeVersion: '15.0.1',
-            buildDir: (process.env.TMPDIR || process.env.TEMP || '/tmp') + '/atom-shell',
-            projectName: 'RecetaCoctel',
-            productName: 'RecetaCoctel'
+            },
+            build: {
+                command: "echo true",
+            },
         }
     });
 
@@ -36,14 +32,13 @@ module.exports = function (grunt) {
 
     // Comprobación de de sintaxis
     grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-build-atom-shell');
 
     //Tareas
 
 
     //Instalación de dependencias o actualización de las existentes
     grunt.registerTask('install', ['shell:npm_install']);
-    grunt.registerTask('build', ["build-atom-shell"]);
+    grunt.registerTask("build", ["shell:build"]);
     //Tarea para ejecución en integración continua o para instalar 
     //dependencias sin modificar el package-lock.json
     grunt.registerTask("ci", ["shell:npm_ci"]);
