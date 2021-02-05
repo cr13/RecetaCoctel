7fe9c067ab49aafd4e2af88679a37e612fe2ca9e
Cristóbal <cristobal111992@gmail.com>
2021-02-05 11:29:54 +0100

    Avanzo #6, corrijo dificultad de test controller_receta.test #35

--- src/test/controller_receta.test.js
+++ src/test/controller_receta.test.js
@@ -10,8 +10,8 @@ const ingrediente_dos = new ingrediente(2, "Seven UP", "200ml", "bebida");
 const ingrediente_tres = new ingrediente(3, "Azucar Morena", "1 o 2 cucharadas", "Hidratos de carbono");
 const ingrediente_cuatro = new ingrediente(4, "Hierba buena", "2-4 hojas", "Flor");
 
-const receta_uno = new receta(1, "Mojito", "Tritura las hojas de menta con el azúcar y el zumo de lima. Agrega un chorrito de agua con gas y llena el vaso con hielo picado. Vierta el ron y cubra con agua con gas. Decore y sirva con pajita.",10, "Fácil", 5, [ingrediente_uno, ingrediente_dos, ingrediente_tres]);
-const receta_dos = new receta(2, "Daikiri", "Tritura las hojas de menta con el azúcar y el zumo de lima. Agrega un chorrito de agua con gas y llena el vaso con hielo picado. Vierta el ron y cubra con agua con gas. Decore y sirva con pajita.",10, "Fácil", 5, [ingrediente_uno, ingrediente_dos, ingrediente_tres, ingrediente_cuatro]);
+const receta_uno = new receta(1, "Mojito", "Tritura las hojas de menta con el azúcar y el zumo de lima. Agrega un chorrito de agua con gas y llena el vaso con hielo picado. Vierta el ron y cubra con agua con gas. Decore y sirva con pajita.",10, "FACIL", 5, [ingrediente_uno, ingrediente_dos, ingrediente_tres]);
+const receta_dos = new receta(2, "Daikiri", "Tritura las hojas de menta con el azúcar y el zumo de lima. Agrega un chorrito de agua con gas y llena el vaso con hielo picado. Vierta el ron y cubra con agua con gas. Decore y sirva con pajita.",10, "FACIL", 5, [ingrediente_uno, ingrediente_dos, ingrediente_tres, ingrediente_cuatro]);
 
 describe("Función para añadir una receta", () => {
     test('debe añadir un receta y comprobamos que efectivamente el diccionario contiene un elemento', () => {
@@ -45,7 +45,7 @@ describe("Función para obtener una receta por titulo", () => {
 
         expect.assertions(listRecetas);
         listRecetas.addReceta(receta_uno);
-        expect(listRecetas.getRecetaByTitle('Mojito')).toEqual({ "comensales": 5, "dificultad": "Fácil", "duracion": 10, "id_receta": 1, "ingredientes": [{ "descripcion": "100ml", "id_ing": 1, "nombre": "Ron Blanco", "recetas": undefined, "tipo": "bebida" }, { "descripcion": "200ml", "id_ing": 2, "nombre": "Seven UP", "recetas": undefined, "tipo": "bebida" }, { "descripcion": "1 o 2 cucharadas", "id_ing": 3, "nombre": "Azucar Morena", "recetas": undefined, "tipo": "Hidratos de carbono" }], "instrucciones": "Tritura las hojas de menta con el azúcar y el zumo de lima. Agrega un chorrito de agua con gas y llena el vaso con hielo picado. Vierta el ron y cubra con agua con gas. Decore y sirva con pajita.", "titulo": "Mojito" });
+        expect(listRecetas.getRecetaByTitle('Mojito')).toEqual({ "comensales": 5, "dificultad": "FACIL", "duracion": 10, "id_receta": 1, "ingredientes": [{ "descripcion": "100ml", "id_ing": 1, "nombre": "Ron Blanco", "recetas": undefined, "tipo": "bebida" }, { "descripcion": "200ml", "id_ing": 2, "nombre": "Seven UP", "recetas": undefined, "tipo": "bebida" }, { "descripcion": "1 o 2 cucharadas", "id_ing": 3, "nombre": "Azucar Morena", "recetas": undefined, "tipo": "Hidratos de carbono" }], "instrucciones": "Tritura las hojas de menta con el azúcar y el zumo de lima. Agrega un chorrito de agua con gas y llena el vaso con hielo picado. Vierta el ron y cubra con agua con gas. Decore y sirva con pajita.", "titulo": "Mojito" });
 
     });
 });
