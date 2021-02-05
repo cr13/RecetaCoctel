e5579602f8a9bbf5b46c5049d4b4014da926bfb0
Cristóbal <cristobal111992@gmail.com>
2021-02-05 11:29:29 +0100

    Avanzo #6, corrijo dificultad de test receta.test #35

--- src/test/receta.test.js
+++ src/test/receta.test.js
@@ -10,9 +10,9 @@ describe("Función para insertar una receta", () => {
         const ingrediente_cuatro = new ingrediente(4, "Hierba buena", "2-4 hojas", "Flor");
 
         const receta_uno = new receta(1, "Mojito", "Tritura las hojas de menta con el azúcar y el zumo de lima. Agrega un chorrito de agua con gas y llena el vaso con hielo picado. Vierta el ron y cubra con agua con gas. Decore y sirva con pajita.",
-        10, "Fácil", 5, [ingrediente_uno, ingrediente_dos, ingrediente_tres]);
+        10, "FACIL", 5, [ingrediente_uno, ingrediente_dos, ingrediente_tres]);
         const receta_dos = new receta(2, "Mojito", "Tritura las hojas de menta con el azúcar y el zumo de lima. Agrega un chorrito de agua con gas y llena el vaso con hielo picado. Vierta el ron y cubra con agua con gas. Decore y sirva con pajita.",
-        15, "Fácil", 5, [ingrediente_uno, ingrediente_dos, ingrediente_tres, ingrediente_cuatro]);
+        15, "FACIL", 5, [ingrediente_uno, ingrediente_dos, ingrediente_tres, ingrediente_cuatro]);
 
         expect(receta_uno).not.toBe(receta_dos);
 
